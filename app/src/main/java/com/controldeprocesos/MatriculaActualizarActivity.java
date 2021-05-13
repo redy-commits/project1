60e203a309629d122ae8eb701b379e6f6adee538
LLuucchhoo <lcortezdominguez@hotmail.com>
2021-05-12 22:13:24 -0600

    Update MatriculaActualizarActivity.java

--- app/src/main/java/com/controldeprocesos/MatriculaActualizarActivity.java
+++ app/src/main/java/com/controldeprocesos/MatriculaActualizarActivity.java
@@ -3,19 +3,6 @@ package com.controldeprocesos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-public class MatriculaActualizarActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_matricula_actualizar);
-    }
-}package com.controldeprocesos;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
