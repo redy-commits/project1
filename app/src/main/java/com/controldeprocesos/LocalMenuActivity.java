62204de6ba58f8d9689d2b76e39b31b0deab9fd2
LLuucchhoo <lcortezdominguez@hotmail.com>
2021-05-17 15:44:03 -0600

    Actualización de CRUD Local

--- app/src/main/java/com/controldeprocesos/LocalMenuActivity.java
+++ app/src/main/java/com/controldeprocesos/LocalMenuActivity.java
@@ -2,13 +2,37 @@ package com.controldeprocesos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class LocalMenuActivity extends AppCompatActivity {
+public class LocalMenuActivity extends ListActivity {
+
+    String[] menu={"Insertar registro","Eliminar registro","Consultar registro","Actualizar registro"};
+    String[] activities={"LocalInsertarActivity","LocalBorrarActivity","LocalConsultarActivity","LocalActualizarActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_local_menu);
-    }
-}
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(0, 0, 255));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);}
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(128, 128, 255));
+
+        try{
+            Class<?> clase=Class.forName("com.controldeprocesos."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);}
+        catch(ClassNotFoundException e){
+            e.printStackTrace();}}}
