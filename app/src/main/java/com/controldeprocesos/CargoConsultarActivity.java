0dcf1d6f852e060bbb4b4ba7b60eb4958ef7f342
LLuucchhoo <lcortezdominguez@hotmail.com>
2021-05-16 19:31:35 -0600

    Actualización del CRUD Cargo

--- app/src/main/java/com/controldeprocesos/CargoConsultarActivity.java
+++ app/src/main/java/com/controldeprocesos/CargoConsultarActivity.java
@@ -2,13 +2,33 @@ package com.controldeprocesos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CargoConsultarActivity extends AppCompatActivity {
+public class CargoConsultarActivity extends Activity {
+
+    private ControlBDGpo16 helper;
+    private EditText edtIdCargo;
+    private TextView txtNombreCargo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargo_consultar);
-    }
-}
+        helper = new ControlBDGpo16(this);
+        edtIdCargo=(EditText)findViewById(R.id.edtIdCargo);
+        txtNombreCargo=(TextView)findViewById(R.id.txtNombreCargo);
+        txtNombreCargo.setText("");}
+
+    public void consultarCargo(View v) {
+        helper.abrir();
+        Cargo cargo = helper.consultarCargo(Integer.parseInt(edtIdCargo.getText().toString()));
+        helper.cerrar();
+        if(cargo == null)
+            Toast.makeText(this, "El cargo "+edtIdCargo.getText().toString()+ " no ha sido encontrado", Toast.LENGTH_LONG).show();
+        else{
+            txtNombreCargo.setText("Nombre de cargo: "+cargo.getNombreCargo());}}}
