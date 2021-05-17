070c44f22ff5c2817b1484bfe7cacb06918003d4
Veronica Borja <bp15001@ues.edu.sv>
2021-05-16 22:51:23 -0600

    Insertar Evaluacion

--- app/src/main/java/com/controldeprocesos/Evaluacion.java
+++ app/src/main/java/com/controldeprocesos/Evaluacion.java
@@ -1,4 +1,73 @@
 package com.controldeprocesos;
 
+import java.util.Date;
+
+
 public class Evaluacion {
+
+    private  int numEva, idDocente, alumnosEvaluados;
+    private String codMateria, tipo;
+    private Date fechaRealizacion, fechaPublicacion;
+
+    public Evaluacion(){}
+
+    public Evaluacion(int numEva, int idDocente, int alumnosEvaluados, String codMateria, String tipo, Date fechaRealizacion, Date fechaPublicacion) {
+        this.numEva = numEva;
+        this.idDocente = idDocente;
+        this.alumnosEvaluados = alumnosEvaluados;
+        this.codMateria = codMateria;
+        this.tipo = tipo;
+        this.fechaRealizacion = fechaRealizacion;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public int getNumEva() {
+        return numEva;
+    }
+    public void setNumEva(int numEva) {
+        this.numEva = numEva;
+    }
+
+    public int getIdDocente() {
+        return idDocente;
+    }
+    public void setIdDocente(int idDocente) {
+        this.idDocente = idDocente;
+    }
+
+    public int getAlumnosEvaluados() {
+        return alumnosEvaluados;
+    }
+    public void setAlumnosEvaluados(int alumnosEvaluados) {
+        this.alumnosEvaluados = alumnosEvaluados;
+    }
+
+    public String getCodMateria() {
+        return codMateria;
+    }
+    public void setCodMateria(String codMateria) {
+        this.codMateria = codMateria;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getFechaRealizacion() {
+        return fechaRealizacion;
+    }
+    public void setFechaRealizacion(Date fechaRealizacion) {
+        this.fechaRealizacion = fechaRealizacion;
+    }
+
+    public Date getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+    public void setFechaPublicacion(Date fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
 }
--- app/src/main/java/com/controldeprocesos/EvaluacionMenuActivity.java
+++ app/src/main/java/com/controldeprocesos/EvaluacionMenuActivity.java
@@ -1,14 +1,41 @@
 package com.controldeprocesos;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import android.os.Bundle;
 
-public class EvaluacionMenuActivity extends AppCompatActivity {
+public class EvaluacionMenuActivity extends ListActivity {
+
+    String[] menu={"Insertar evaluacion","Eliminar evaluacion","Consultar evaluacion","Actualizar evaluacion"};
+    String[] activities={"EvaluacionInsertarActivity","EvaluacionBorrarActivity","EvaluacionConsultarActivity","EvaluacionActualizarActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_evaluacion_menu);
+
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(235,86,163));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+
     }
-}
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+
+        String nombreValue=activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(128, 128, 255));
+
+        try{
+            Class<?> clase=Class.forName("com.controldeprocesos."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);}
+        catch(ClassNotFoundException e){
+            e.printStackTrace();}}}
