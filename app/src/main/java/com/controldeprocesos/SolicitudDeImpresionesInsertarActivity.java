3e4dcf7da18b16390a7456c934151c946cf34040
vborjap <bp15001@ues.edu.sv>
2021-06-04 21:09:02 -0600

    servicio web para insertar solicitud de cambio

--- app/src/main/java/com/controldeprocesos/SolicitudDeImpresionesInsertarActivity.java
+++ app/src/main/java/com/controldeprocesos/SolicitudDeImpresionesInsertarActivity.java
@@ -2,8 +2,10 @@ package com.controldeprocesos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,11 +16,16 @@ public class SolicitudDeImpresionesInsertarActivity extends Activity {
     EditText edtIdSolicitud;
     EditText edtNumPaginas;
     EditText edtEstadoAprobado;
+    //Para el servicio web
+    private String urlS= "https://cn18006pdm115.000webhostapp.com/p1/insertarSolicitudDeImpresiones.php";
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solicitud_de_impresiones_insertar);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         helper = new ControlBDGpo16(this);
         edtIdSolicitud = (EditText) findViewById(R.id.edtIdSolicitud);
         edtNumPaginas = (EditText) findViewById(R.id.edtNumPaginas);
@@ -31,6 +38,7 @@ public class SolicitudDeImpresionesInsertarActivity extends Activity {
         int idSolicitud = Integer.parseInt(edtIdSolicitud.getText().toString());
         int numPaginas = Integer.parseInt(edtNumPaginas.getText().toString());
         boolean estadoAprobado=Boolean.parseBoolean(edtEstadoAprobado.getText().toString());
+
         solicitud.setIdSolicitud(idSolicitud);
         solicitud.setNumPaginas(numPaginas);
         solicitud.setEstadoAprobado(estadoAprobado);
@@ -39,6 +47,10 @@ public class SolicitudDeImpresionesInsertarActivity extends Activity {
         helper.cerrar();
         Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
 
+        //servicio de consumo web.
+        String url = null;
+        url = urlS+ "?idSolicitud=" +idSolicitud+ "&numPaginas="+numPaginas+ "&estadoAprobado="+estadoAprobado;
+        ControladorServicio.insertar(url, this);
     }
     public void limpiarTexto(View v) {
 
